4f114d3b15cbdab5e886eb36b51cd641e5db8316
Kayla Lebron <kayla.lebron12@gmail.com>
2023-04-17 18:01:53 -0400

    wassup

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    
+  }
+  return messages
+}
+//writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+function countDown() {
+    var i = 10;
+    while (i >= 0) {
+        console.log(i--)
+    }
+
+}
